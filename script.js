99f97ddaeb54474e9a93ab06b795678c8bd442dc
asif <asif@gmail.com>
2022-01-31 13:26:50 +0600

    change

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
 //firstclass ticket
-
 document.getElementById("fr-plus").addEventListener("click",function(){
-    let id=document.getElementById("frTicket");
-    let parid=parsInt(id.value);
-    let result=parid+1;
-    id.value=result;
+    ticketNumber(true)
 })
+
+document.getElementById("fr-minus").addEventListener("click",function(){
+    ticketNumber(false)
+})
+
+function ticketNumber(increase){
+    const frTicket=document.getElementById("frTicket");
+    let parFr=parseInt(frTicket.innerText);
+    
+    if(increase==true){
+        parFr=parFr+1;
+    }
+    if(increase==false && parFr>0){
+        parFr=parFr-1;
+    }
+    //frTicket.innerText=parFr;
+}
+
+
+
+function ticketNumber(increase){
+    let frTicket= parseInt(document.getElementById("frTicket").innerText);
+    if(increase==true){
+        frTicket=frTicket+1;
+    }
+    if(increase==false && frTicket>0){
+        frTicket=frTicket-1;
+    }
+
+    document.getElementById("frTicket").innerText=frTicket;
+}
