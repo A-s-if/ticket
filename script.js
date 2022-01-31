99f719f0b6975b03bfb2aff37e460174ebb0b526
asif <asif@gmail.com>
2022-01-31 22:25:53 +0600

    change

--- script.js
+++ script.js
@@ -1,35 +1,39 @@
 //firstclass ticket
-document.getElementById("fr-plus").addEventListener("click",function(){
-    ticketNumber(true)
-})
 
-document.getElementById("fr-minus").addEventListener("click",function(){
-    ticketNumber(false)
-})
-
-function ticketNumber(increase){
-    const frTicket=document.getElementById("frTicket");
-    let parFr=parseInt(frTicket.innerText);
-    
+function ticketNumber(ticket,increase){
+    let frTicket= document.getElementById(ticket);
+    let parfrTicket=parseInt(frTicket.innerText);
     if(increase==true){
-        parFr=parFr+1;
+        parfrTicket=parfrTicket+1;
     }
-    if(increase==false && parFr>0){
-        parFr=parFr-1;
+    if(increase==false && parfrTicket>0){
+        parfrTicket=parfrTicket-1;
     }
-    //frTicket.innerText=parFr;
+
+    frTicket.innerText=parfrTicket;
+    tk()
 }
 
+function tk(){
+    let frTk=parseInt(document.getElementById("frTicket").innerText);
+    let resultfrTk=frTk*5000;
+    document.getElementById("frTk").innerText=resultfrTk;
+    let scTk=parseInt(document.getElementById("scTicket").innerText);
+    let resultScTk=scTk*3000;
+    document.getElementById("scTk").innerText=resultScTk;
+    let trTk=parseInt(document.getElementById("trTicket").innerText);
+    let resultTrTk=trTk*2000;
+    document.getElementById("trTk").innerText=resultTrTk;
+    
+    let frTax=resultfrTk*0.1;
+    document.getElementById("frVat").innerText=frTax;
 
+    let scTax=resultScTk*0.1;
+    document.getElementById("scVat").innerText=scTax;
 
-function ticketNumber(increase){
-    let frTicket= parseInt(document.getElementById("frTicket").innerText);
-    if(increase==true){
-        frTicket=frTicket+1;
-    }
-    if(increase==false && frTicket>0){
-        frTicket=frTicket-1;
-    }
+    let trTax=resultTrTk*0.1;
+    document.getElementById("trVat").innerText=trTax;
 
-    document.getElementById("frTicket").innerText=frTicket;
+    let total=resultfrTk+resultScTk+resultTrTk+frTax+scTax+trTax;
+    document.getElementById("totalTk").innerText=total;
 }
